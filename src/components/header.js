281558684b002d33775e27dae4a4c2df61cb77b0
Maciej Skierkowski <maciej@skierkowski.com>
2019-04-04 23:01:16 -0700

    cleanup

--- src/components/header.js
+++ src/components/header.js
@@ -3,32 +3,22 @@ import PropTypes from "prop-types"
 import React from "react"
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+  <div class="ui secondary pointing menu">
+    <a class="active item">
+      Home
+    </a>
+    <a class="item">
+      Messages
+    </a>
+    <a class="item">
+      Friends
+    </a>
+    <div class="right menu">
+      <a class="ui item">
+        Logout
+      </a>
     </div>
-  </header>
+  </div>
 )
 
 Header.propTypes = {
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,18 @@
 import React from "react"
-import { Link } from "gatsby"
+// import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
+// import Image from "../components/image"
 import SEO from "../components/seo"
+
+import "semantic-ui-less/semantic.less";
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <div class="ui container">
+      <div>Hi</div>
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 
